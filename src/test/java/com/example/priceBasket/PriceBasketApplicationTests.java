c0c1fedd8b48f86d879372edfbfbbee895e9b173
David Dane <danedavid46@gmail.com>
2022-05-16 08:32:57 +0300

    Init project

---
+++ src/test/java/com/example/priceBasket/PriceBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priceBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
